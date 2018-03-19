cb7ffd288d08902dd9b118cb484255ffd4bf0866
Chase Gatyas <MagRat04@users.noreply.github.com>
2018-03-19 14:00:43 -0500

    hard coding the minesweeper example

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+const blankLIne = ' | | ';
+const guessLine = '1| | ';
+const bombLine = ' |B| ';
+
+console.log("This is what my empty board would look like: ");
+console.log(blankLIne);
+console.log(blankLIne);
+console.log(blankLIne); 
+
+console.log("This is what a board with a guess and a bomb would look like: ");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLIne); 
